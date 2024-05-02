f30cc6ed1058e8bc8780343ab266729f5688df66
lyh <beta2020@naver.com>
2024-05-02 10:59:23 +0900

    Feat: Create product posting function

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+app_name = "products"
+
+
+# ViewSet을 Router에 등록
+router = DefaultRouter()
+router.register("", views.ProductViewSet, basename='product')
+urlpatterns = router.urls
--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import (viewsets, status, permissions)
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
